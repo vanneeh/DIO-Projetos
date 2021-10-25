5bc35dfe872dd2d889cd73e813136bca9e44556a
vanneeh <49892008+vanneeh@users.noreply.github.com>
2021-10-25 20:11:13 -0300

    Add files via upload

---
+++ DIO-NetflixClone/README.md
@@ -0,0 +1,3 @@
+# "Recriando a interface do Netflix"
+
+Para o projeto do curso HTML Web Developer, criei a minha versão do Netflix: o Asianflix, que é uma página voltada para dramas, filmes e shows de variedades de origem asiática.
---
+++ DIO-NetflixClone/js/Owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 8,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        400: {
+            items: 2
+        },
+        700: {
+            items: 4
+        },
+        1100: {
+            items: 5
+        },
+        1300: {
+            items: 6
+        }
+    }
+})
